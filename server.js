79687d2e51d99ab0aa1bf0fe3eafb1b67ea89fd1
FamutimiEmmanuel <olamide.famutimi@gmail.com>
2022-10-02 13:32:32 -0700

    prepare to deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 connectDB();
@@ -8,12 +9,19 @@ connectDB();
 app.use(cors());
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the school portal'}));
+// app.get('/', (req, res) => res.json({ msg: 'Welcome to the school portal'}));
 
 app.use(require('./routes/auth'))
 app.use(require('./routes/staffs'))
 app.use(require('./routes/students'))
 app.use(require('./routes/admin'))
+
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
